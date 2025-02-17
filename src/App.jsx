ac787a72a013eef27a3909637fa6a936b5cad5c8
mohammadHossein <mohammadhossdinahmadian@gmail.com>
2025-02-17 13:19:14 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import { routes } from './routes/index.jsx';
+import "./index.css";
+
+function App() {
+  return (
+    <Routes>
+      {Object.keys(routes).map((route) => {
+        return (
+          <Route
+            element={routes[route].element}
+            path={routes[route].path}
+          />
+        );
+      })}
+    </Routes>
+  )
+}
+
+export default App
